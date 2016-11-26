7c31f034b1b5ff7bfbf66cc688ad11f19e5a6f9a
wlj <58455411@163.com>
2016-11-26 11:22:52 +0800

    switch

--- app/src/main/java/com/hd/wlj/duohaowan/LoadingActivity.java
+++ app/src/main/java/com/hd/wlj/duohaowan/LoadingActivity.java
@@ -1,28 +1,19 @@
 package com.hd.wlj.duohaowan;
 
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import com.bumptech.glide.Glide;
 import com.hd.wlj.duohaowan.been.Load;
+import com.wlj.base.bean.Banner;
 import com.wlj.base.bean.Base;
+import com.wlj.base.util.DpAndPx;
+import com.wlj.base.util.GoToHelp;
 import com.wlj.base.web.asyn.AsyncCall;
+import com.wlj.base.widget.AutoScrollViewPager;
+import com.wlj.base.widget.SwitchViewPager;
 
 import org.json.JSONObject;
 
@@ -31,45 +22,56 @@ import java.util.List;
 
 public class LoadingActivity extends AppCompatActivity {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
-    private ArrayList<String> mData;
+    private List<String> mData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loading);
 
+//        setContentView(R.layout.activity_loading);
+//
         mData = new ArrayList<>();
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(),mData);
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-
         Load load = new Load(this);
-
         load.Request().setOnAsyncBackListener(new AsyncCall.OnAsyncBackListener() {
             @Override
             public void OnAsyncBack(List<Base> list, Base base, int requestType) {
+
                 for (Base base1 : list) {
                     JSONObject jsonObject = base1.getResultJsonObject();
                     String pic = jsonObject.optString("pic");
                     mData.add(Urls.HOST+pic);
                 }
-                mSectionsPagerAdapter.notifyDataSetChanged();
+
+                SwitchViewPager<String> switchViewPager = new SwitchViewPager<String>(LoadingActivity.this,mData){
+                    /**
+                     * 给滑视图添加页面
+                     *
+                     * @param autoviewPager
+                     */
+                    @Override
+                    protected void addSwitchPage(AutoScrollViewPager autoviewPager) {
+                        super.addSwitchPage(autoviewPager);
+
+                        setScrollLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+
+                        autoviewPager.stopAutoScroll();
+                        autoviewPager.setCycle(false);
+                    }
+
+                    @Override
+                    public void onPageSelected(int position) {
+                        super.onPageSelected(position);
+
+                        if( position%mData.size() >= mData.size()-1 ){
+
+                            GoToHelp.go(LoadingActivity.this,MainActivity.class);
+                            finish();
+                        }
+
+                    }
+                };
+                View createview = switchViewPager.createview();
+                setContentView(createview);
             }
 
             @Override
@@ -79,115 +81,8 @@ public class LoadingActivity extends AppCompatActivity {
         });
 
 
+
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_loading, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String IMAGEURL = "url";
-        private static final String POSTION = "position";
-
-        public PlaceholderFragment() {
-
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(String url, int position) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(POSTION,position);
-            args.putString(IMAGEURL, url);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-//            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            String url = getArguments().getString(IMAGEURL);
-            int position = getArguments().getInt(POSTION);
-
-
-            ImageView imageView1 = new ImageView(getContext());
-
-            imageView1.setAdjustViewBounds(true);
-            imageView1.setScaleType(ImageView.ScaleType.FIT_XY);
-            Glide.with(this).
-                    load(url)
-                    .placeholder(R.drawable.project_bg)
-                    .into(imageView1);
-            return imageView1;
-        }
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        private final List<String> list;
-
-        public SectionsPagerAdapter(FragmentManager fm, List<String> list) {
-            super(fm);
-            this.list = list;
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(list.get(position),position);
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return list.size();
-        }
-
-//        @Override
-//        public CharSequence getPageTitle(int position) {
-//            switch (position) {
-//                case 0:
-//                    return "SECTION 1";
-//                case 1:
-//                    return "SECTION 2";
-//                case 2:
-//                    return "SECTION 3";
-//            }
-//            return null;
-//        }
-    }
 }
